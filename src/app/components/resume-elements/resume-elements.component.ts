966dc7fafb178993b168cd0634240bbb0487fc2c
Fred-Redstone-2 <149532029+Fred-Redstone-2@users.noreply.github.com>
2026-06-27 22:38:56 -0400

    feat: Ajout des projets au CV

--- src/app/components/resume-elements/resume-elements.component.ts
+++ src/app/components/resume-elements/resume-elements.component.ts
@@ -3,6 +3,7 @@ import { Component, computed, Signal } from '@angular/core';
 import { LinkComponent } from '@app/components/link/link.component';
 import { ResumeSectionComponent } from '@app/components/resume-section/resume-section.component';
 import { UrlComponent } from '@app/components/url/url.component';
+import { PillListComponent } from '@app/components/pill-list/pill-list.component';
 import { SimpleMarkdownPipe } from '@app/utilities/simple-markdown.pipe';
 
 import { CONTACTS, EMAIL, RESUME, RESUME_END_MESSAGE } from '@app/data-structures/constants';
@@ -12,7 +13,7 @@ import { TechSkills } from '@app/data-structures/tech-skills';
 
 @Component({
   selector: 'app-resume-elements',
-  imports: [LinkComponent, ResumeSectionComponent, UrlComponent, SimpleMarkdownPipe],
+  imports: [LinkComponent, ResumeSectionComponent, UrlComponent, PillListComponent, SimpleMarkdownPipe],
   templateUrl: './resume-elements.component.html',
 })
 export class ResumeElementsComponent {
